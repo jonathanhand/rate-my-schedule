8771e64bc9717dd460cbc44cad4d7541798e6d91
jonathanhand <33673845+jonathanhand@users.noreply.github.com>
2018-11-04 10:59:51 -0800

    functioning parseTest
    creates a DB and parses the csulb schedule data into it

--- parsertest.py
+++ parsertest.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import unicodedata
 import sqlite3
 
+#DB stuff
 conn=sqlite3.connect('IScourses.db')
 print("Connection opened")
 
@@ -11,19 +12,17 @@ cur=conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS InfoSystems') #drop existing table
 #make new table
-cur.execute('''CREATE TABLE InfoSystems (CNum INTEGER, Days TEXT, Time TEXT, Location TEXT, Professor TEXT);''')
+cur.execute('''CREATE TABLE InfoSystems (courseCode TEXT,CNum INTEGER, noCost TEXT, reserve TEXT, notes TEXT, \
+type TEXT, Days TEXT, Time TEXT, openSeats TEXT, Location TEXT, Professor TEXT, comment TEXT);''')
 #passing hardcoded values
-cur.execute('''INSERT INTO InfoSystems (CNum, Days, Time, Location, Professor) \
-    VALUES(4324,'MW','8-9:15AM','PSY-154', 'Thomason A')''');
+#cur.execute('''INSERT INTO InfoSystems (CNum, noCost, reserve, notes, type, Days, Time, openSeats, Location, Professor, comment) \
+    #VALUES(4324,null,null,null,null,null,null,'MW','8-9:15AM','PSY-154', 'Thomason A', 'ISCODE')''');
 cNumTest= 190
 #format to pass variables
-cur.execute('''Insert INTO InfoSystems VALUES (?,'TR','9-10','CBA101','James')''', [(cNumTest)]);
-
-cur = conn.execute("SELECT * from InfoSystems")
-for row in cur:
-    print(row)
 
 
+
+#beautiful soup stuff
 address = "http://web.csulb.edu/depts/enrollment/registration/class_schedule/Spring_2019/By_Subject/IzS.html"
 response = urllib.request.urlopen(address)
 bytes = response.read()
@@ -33,15 +32,12 @@ htmlText = open("csulbhtml.txt", "w")
 
 soup = BeautifulSoup(text, 'html5lib')
 
-print("------------------------------------------")
+print("NEW RUN------------------------------------------")
 courseCode=soup.select("[class~=courseCode]")
 courseTitle=soup.select("[class~=courseTitle]")
 
 
-#rows = soup.find("[scope~=row]")
-
 table = soup.find_all('table', 'sectionTable')
-#divCourseHeader = soup.find('div', 'courseHeader').get_text()
 divCourseHeader = soup.find('div', 'courseHeader')
 siblingHeader = divCourseHeader.next_child
 spanCode = soup.find('span', 'courseCode')
@@ -50,54 +46,69 @@ allCourseCodeDiv = soup.find_all('div', 'courseHeader')
 
 
 
-#tableTitle = list(soup.divCourseHeader.children)
 td = soup.table('td')
-#text = soup.table.find_next_sibling('td').get_text()
 
 
 block = soup.td.findAll('td')
 
 
 data = []
-#print(courseCode[0].get_text())
-#print(divCourseHeader)
-#print(siblingHeader)
-#print(spanCode)
-#print("NEXT: ",nextSpan)
+
 
 courseBlockDivs = soup.find_all('div', 'courseBlock')
-classNumDict = dict()
-subjectClassDict = dict()
+
 classesList = []
-counter = 0
+counter=0
 for div in courseBlockDivs:
     courseCodeSpan = div.find('span', 'courseCode').get_text()
     sectionTable = div.find('table', 'sectionTable')
-    print(courseCodeSpan)
-    print("____________")
+    print("----------------")
     trRows = sectionTable.find_all('tr')
+    counter=0
+    for row in trRows: #for each table
+        counter+=1
+        if counter > 1:
+            classesList.append(courseCodeSpan)
+        tdRows = row.find_all('td') #find all td in table
+        for i in tdRows: #loops through each indiviudal td
+            classesList.append(i.get_text()) #adds text in each td to a list
 
-    counter = 0
-    for row in trRows: #FIXME ENDED HERE 10/30. classes for each column?
-        tdRows = row.find_all('td')
-       
-        for i in tdRows:
-            print(i)
-            c=0
-        
-        #go by section number to help counter??
-        counter=0
-        #for td in tdRows:
-            #print(counter)
-            #counter+=1
-    
-    
 
-    
-#cur.executemany('INSERT INTO InfoSystems values (?,?,?,?,?,?,?,?,?,?,?)', (classesList[0],))
+            
+#runs through class list and changes all empty values to null
+for n, i in enumerate(classesList):
+    if i == '':
+        classesList[n] = None
+    elif i == '\xa0':
+        classesList[n] = None
+
+i=0
+while i <len(classesList):
+    cur.execute('INSERT INTO InfoSystems VALUES(?,?,?,?,?,?,?,?,?,?,?, ?)' , [classesList[i],
+                                                                            classesList[i+1],
+                                                                            classesList[i+2],
+                                                                            classesList[i+3],
+                                                                            classesList[i+4],
+                                                                            classesList[i+5],
+                                                                            classesList[i+6],
+                                                                            classesList[i+7],
+                                                                            classesList[i+8],
+                                                                            classesList[i+9],
+                                                                            classesList[i+10],
+                                                                           classesList[i+11]]);
+    i+=12
+
 conn.commit()
-        #print(row.get_text(), end="\n")
+cur = conn.execute("SELECT CNum, professor from InfoSystems where courseCode='I S 300'")
+for row in cur:
+    print(row)
     
+#while i < len(classesList)+counter:
+    #cur.executemany('INSERT INTO InfoSystems values (?,?,?,?,?,?,?,?,?,?,?)', (classesList[i],classesList[i+1],classesList[i+2],classesList[i+3],classesList[i+4],classesList[i+5],classesList[i+6],classesList[i+7],classesList[i+8],classesList[i+9],classesList[i+10]));
+    #i+=11
+#conn.commit()
+#cur = conn.execute("SELECT * from InfoSystems")
+
             
     
         
@@ -117,6 +128,7 @@ while '' in data: #strip the blank spaces
     data.remove('')
 while '\xa0' in data: #strip the unicode transferred spaces
     data.remove('\xa0')
+    
     
 
 dCourseWCode=dict()     
